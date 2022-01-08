6f01406d36c5179971d4b93349d2468c957b3024
Marcin Zlotek <zlociu@onet.eu>
2022-01-08 16:00:50 +0100

    improve run.sh, add creating keyspace fn

--- Matchmaker/Program.cs
+++ Matchmaker/Program.cs
@@ -5,8 +5,15 @@ using Cassandra;
 Console.WriteLine("Hello, World!");
 
 var cluster = Cluster.Builder()
-                     .AddContactPoint("srds")
+                     .AddContactPoint("0.0.0.0:9042")
                      .Build();
 
 
+var tmp = cluster.Connect();
+
+tmp.CreateKeyspaceIfNotExists("matchmaker", new Dictionary<string, string>{ { "class", ReplicationStrategies.SimpleStrategy }, { "replication_factor", "3./" } });
+
 var session = cluster.Connect("matchmaker");
+
+
+
--- cassandraRun.sh
+++ cassandraRun.sh
@@ -1,3 +1,11 @@
-docker run --name srds -e CASSANDRA_CLUSTER_NAME=brak_nazwy -d cassandra:latest
-docker run --name srds-2 -e CASSANDRA_SEEDS=srds,srds-2 -e CASSANDRA_CLUSTER_NAME=brak_nazwy cassandra:latest
-docker run --name srds-3 -e CASSANDRA_SEEDS=srds,srds-2 -e CASSANDRA_CLUSTER_NAME=brak_nazwy cassandra:latest
+docker run --name srds -e CASSANDRA_CLUSTER_NAME=brak_nazwy -p 9042:9042 -d cassandra:latest
+INSTANCE1=$(docker inspect --format='{{ .NetworkSettings.IPAddress }}' srds)
+echo "Instance 1: ${INSTANCE1}"
+
+docker run --name srds-2 -p 9043:9042 -d -e CASSANDRA_CLUSTER_NAME=brak_nazwy -e CASSANDRA_SEEDS=$INSTANCE1 cassandra:latest
+INSTANCE2=$(docker inspect --format='{{ .NetworkSettings.IPAddress }}' srds-2)
+echo "Instance 2: ${INSTANCE2}"
+
+docker run --name srds-3 -p 9044:9042 -d -e CASSANDRA_CLUSTER_NAME=brak_nazwy -e CASSANDRA_SEEDS=$INSTANCE1 cassandra:latest
+INSTANCE3=$(docker inspect --format='{{ .NetworkSettings.IPAddress }}' srds-3)
+echo "Instance 2: ${INSTANCE3}"
